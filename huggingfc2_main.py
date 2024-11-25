726d6b3c6d0ababdc15160cbdcc0cce33b7869f0
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-25 14:50:04 +0100

    Update huggingfc2_main.py

--- huggingfc2_main.py
+++ huggingfc2_main.py
@@ -1,1 +1,11 @@
 import os
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_huggingface import HuggingFaceEndpoint
+
+#os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+
+st.title("HuggingFace Project")
+st.write("HF:",HUGGINGFACEHUB_API_TOKEN)
