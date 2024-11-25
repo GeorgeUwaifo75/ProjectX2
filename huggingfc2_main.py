4c9c7f40d7407cba312966bd0dd7985289d47aea
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-25 15:16:20 +0100

    Update huggingfc2_main.py

--- huggingfc2_main.py
+++ huggingfc2_main.py
@@ -9,9 +9,9 @@ load_dotenv()
 HUGGINGFACEHUB_API_TOKEN= os.environ["HUGGINGFACEHUB_API_TOKEN"]
 
 st.title("HuggingFace Project")
-st.write("HF:",HUGGINGFACEHUB_API_TOKEN)
+#st.write("HF:",HUGGINGFACEHUB_API_TOKEN)
 
 llm =  HuggingFaceEndpoint(repo_id="mistralai/Mistral-Nemo-Instruct-2407")
 
-#out = llm.predict("Write a short poem on India")
+out = llm.invoke("Write a short poem on India")
 #st.write(out)
