2061a1f1a20b4c7fbef39eda5170e5d967e7ec02
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-25 23:59:30 +0100

    Update huggingfc2_main.py

--- huggingfc2_main.py
+++ huggingfc2_main.py
@@ -22,19 +22,20 @@ json_data = response.content
 urls = []
 
 
-def setup1(mycontext):
+def setup1():
     # Load the question-answering pipeline
     qa_pipeline = pipeline("question-answering", model='bert-base-uncased')
     
     # Define the context related to GeeksforGeeks
     #context = f"{mycontext}"
-    context = """
-    GeeksforGeeks is a website that provides a wealth of resources for computer science enthusiasts and professionals.
-    It offers articles, tutorials, and coding challenges on a variety of topics including algorithms, data structures, machine learning, and web development.
-    The platform is designed to help users improve their coding skills and prepare for technical interviews.
-    GeeksforGeeks also features a community where users can ask questions, share knowledge, and participate in discussions.
-    """
-    return context
+
+    #context = """
+    #GeeksforGeeks is a website that provides a wealth of resources for computer science enthusiasts and professionals.
+    #It offers articles, tutorials, and coding challenges on a variety of topics including algorithms, data structures, machine learning, and web development.
+    #The platform is designed to help users improve their coding skills and prepare for technical interviews.
+    #GeeksforGeeks also features a community where users can ask questions, share knowledge, and participate in discussions.
+    #"""
+    #return context
 
 #Upload IvieAI dataset
 def upload_ivieAi():
@@ -60,7 +61,7 @@ def main():
 
     st.title("HuggingFace Project")
     mycontext = upload_ivieAi()
-    mycontext = setup1(mycontext)
+    setup1()
 
     # Load the question-answering pipeline
     qa_pipeline = pipeline("question-answering", model='bert-base-uncased')
@@ -72,7 +73,8 @@ def main():
     user_question = st.text_input("Ask a question about your documents:")
     # Ask a question
     if user_question:
-        #st.write("Q:",user_question)
+        st.write(mycontext[0:50])
+    
         #handle_userinput(user_question)
         result = qa_pipeline(question=user_question, context=mycontext)
         st.write(f"Q: {user_question}\nA: {result['answer']}\n")
