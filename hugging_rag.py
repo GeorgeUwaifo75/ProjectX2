f0dc0333e1ff574fb585120186396e0d6335c9ec
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-26 11:17:06 +0100

    Update hugging_rag.py

--- hugging_rag.py
+++ hugging_rag.py
@@ -19,3 +19,10 @@ data = loader.load()
 
 # Display the first 15 entries
 data[:2]
+
+# Create an instance of the RecursiveCharacterTextSplitter class with specific parameters.
+# It splits text into chunks of 1000 characters each with a 150-character overlap.
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+
+# 'data' holds the text you want to split, split the text into documents using the text splitter.
+docs = text_splitter.split_documents(data)
