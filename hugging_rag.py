ddb19749177273bfebc6be4192aaf591c5ae8a02
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-26 12:10:13 +0100

    Update hugging_rag.py

--- hugging_rag.py
+++ hugging_rag.py
@@ -63,8 +63,23 @@ searchDocs = db.similarity_search(question)
 st.write(searchDocs[0].page_content)
 
 #Using a LLM for Tokenizer and Question and Answer purposes
-# Create a tokenizer object by loading the pretrained "Intel/dynamic_tinybert" tokenizer.
-tokenizer = AutoTokenizer.from_pretrained("Intel/dynamic_tinybert")
+# Specify the model name you want to use
+model_name = "Intel/dynamic_tinybert"
 
-# Create a question-answering model object by loading the pretrained "Intel/dynamic_tinybert" model.
-model = AutoModelForQuestionAnswering.from_pretrained("Intel/dynamic_tinybert")
+# Load the tokenizer associated with the specified model
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=512)
+
+# Define a question-answering pipeline using the model and tokenizer
+question_answerer = pipeline(
+    "question-answering", 
+    model=model_name, 
+    tokenizer=tokenizer,
+    return_tensors='pt'
+)
+
+# Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
+# with additional model-specific arguments (temperature and max_length)
+llm = HuggingFacePipeline(
+    pipeline=question_answerer,
+    model_kwargs={"temperature": 0.7, "max_length": 512},
+)
