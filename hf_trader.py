8a0cde23e1caf0b1aff7aa9e5cd9a2d281f24853
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-29 23:41:22 +0100

    Create hf_trader.py

---
+++ hf_trader.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
+
+from huggingface_hub import login
+login("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")
+
+st.write("The Predictor")
+
+#PROMPT = "[INST]YOUR PROMPT HERE[/INST]"
+PROMPT = "[INST]When can I buy ethereum?[/INST]"
+MAX_LENGTH = 32768  # Do not change
+DEVICE = "cpu"
+
+
+model_id = "agarkovv/CryptoTrader-LM"
+base_model_id = "mistralai/Ministral-8B-Instruct-2410"
+
+model = AutoPeftModelForCausalLM.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(base_model_id)
+
+model = model.to(DEVICE)
+model.eval()
+inputs = tokenizer(
+    PROMPT, return_tensors="pt", padding=False, max_length=MAX_LENGTH, truncation=True
+)
+inputs = {key: value.to(model.device) for key, value in inputs.items()}
+
+res = model.generate(
+    **inputs,
+    use_cache=True,
+    max_new_tokens=MAX_LENGTH,
+)
+output = tokenizer.decode(res[0], skip_special_tokens=True)
+st.write(output)
